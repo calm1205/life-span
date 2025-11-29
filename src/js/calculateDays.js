3eda42d87a97736c9d530b222fa6f808ca3e8d2e
calm1205 <nagi.moriyama@mntsq.com>
2025-11-29 21:33:08 +0900

    fix: 定数化

--- src/js/calculateDays.js
+++ src/js/calculateDays.js
@@ -1,5 +1,8 @@
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24
+
+// 生年月日から現在までの経過日数を計算
 export function calculateDays(birthDate, today = new Date()) {
   const diffTime = Math.abs(today.getTime() - birthDate.getTime())
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+  const diffDays = Math.ceil(diffTime / MILLISECONDS_PER_DAY)
   return diffDays
 }
